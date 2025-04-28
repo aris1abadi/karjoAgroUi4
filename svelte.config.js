b5a0cd199a1c10eaab760a5c4a1df5a60fea069c
aris1abadi <aris1abadi@gmail.com>
2025-04-28 22:34:18 +0700

    modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
     kit: {
         adapter: adapter({
-            fallback: 'index.html'
+            fallback: 'index.html',
+            pages: 'build',
+			assets: 'build',			
+			precompress: false,
+			strict: true
         })
     }
 };
